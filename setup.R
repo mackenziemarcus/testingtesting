406133867ded6908dc279187b6edba8caa172e90
mackenziemarcus <122833458+mackenziemarcus@users.noreply.github.com>
2023-01-24 18:50:56 -0700

    R project add

---
+++ setup.R
@@ -0,0 +1,26 @@
+# packageLoad function setup
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+packageLoad('tidyverse')
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgdal')
+
+packageLoad(packages)
+
+#
